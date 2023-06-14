24d31209bdade276a8bf07f4729bebba57e2b54c
eran blank <eranblank533@campus.masterschool.co>
2023-06-14 22:10:33 +0300

    changegit add --all

--- app.py
+++ app.py
@@ -1,18 +1,35 @@
+import uuid
+
 from flask import Flask, render_template, request, redirect, url_for
 import json
 
 app = Flask(__name__)
 
+def read_blog_post():
+    with open('data.json', 'r') as file:
+        blog_posts = json.load(file)
+    return blog_posts
+
 def fetch_post_by_id(post_id):
-    with open('data.json', 'r') as f:
-        blog_posts = json.load(f)
+    """
+    Fetches a blog post by its ID from a JSON file.
+
+    Args:
+        post_id (int): The ID of the blog post to fetch.
+
+    Returns:
+        dict or None: A dictionary representing the blog post if found,
+                      or None if the post with the given ID doesn't exist.
+
+    """
+    blog_posts = read_blog_post()
     for post in blog_posts:
         if post["id"] == post_id:
             return post
     return None
+
 def update_post(post_id, post_content):
-    with open('data.json', 'r') as f:
-        blog_posts = json.load(f)
+    blog_posts = read_blog_post()
     for post in blog_posts:
         if post["id"] == post_id:
             post["content"] = post_content
@@ -21,45 +38,47 @@ def update_post(post_id, post_content):
 
 @app.route('/')
 def index():
-    with open('data.json', 'r') as f:
-        blog_posts = json.load(f)
+    blog_posts = read_blog_post()
     return render_template('index.html', posts=blog_posts)
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
-        with open('data.json', 'r') as f:
-            blog_posts = json.load(f)
-            post_id = len(blog_posts) + 1
-            author = request.form.get('name')
-            title = request.form.get('title')
-            content = request.form.get('content')
-            new_post = {
-                'id': post_id,
-                'author': author,
-                'title': title,
-                'content': content,
-                'likes': 0
+        blog_posts = read_blog_post()
+        author = request.form.get('name')
+        title = request.form.get('title')
+        content = request.form.get('content')
+        new_post = {
+            'id': str(uuid.uuid4()),
+            'author': author,
+            'title': title,
+            'content': content,
+            'likes': 0
             }
-            blog_posts.append(new_post)
+        blog_posts.append(new_post)
         with open('data.json', 'w') as f:
             json.dump(blog_posts, f)
         return redirect(url_for('index'))
     return render_template('add.html')
 
-@app.route('/delete/<int:post_id>', methods=['POST'])
+@app.route('/delete/<string:post_id>', methods=['POST'])
 def delete(post_id):
-    with open('data.json', 'r') as f:
-        blog_posts = json.load(f)
-        for post in blog_posts:
-            if post['id'] == post_id:
-                blog_posts.remove(post)
-                break
+    flag = True
+    blog_posts = read_blog_post()
+    for post in blog_posts:
+        if post['id'] == post_id:
+            blog_posts.remove(post)
+            flag = True
+            break
+        else:
+            flag = False
+    if not flag:
+        raise Exception("Sorry, post does not exist")
     with open('data.json', 'w') as f:
         json.dump(blog_posts, f)
     return redirect(url_for('index'))
 
-@app.route('/update/<int:post_id>', methods=['GET', 'POST'])
+@app.route('/update/<string:post_id>', methods=['GET', 'POST'])
 def update(post_id):
     post = fetch_post_by_id(post_id)
     if post is None:
@@ -70,7 +89,7 @@ def update(post_id):
         return redirect(url_for('index'))
     return render_template('update.html', post=post)
 
-@app.route('/like/<int:post_id>', methods=['POST'])
+@app.route('/like/<string:post_id>', methods=['POST'])
 def like(post_id):
     with open('data.json', 'r') as f:
         blog_posts = json.load(f)
@@ -84,4 +103,3 @@ def like(post_id):
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug = True)
-
