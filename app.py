2a163b71a84198b76c2dbe1ffa7e7006cae29b22
eran blank <eranblank533@campus.masterschool.co>
2023-06-17 13:49:51 +0300

    add--all

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ import json
 app = Flask(__name__)
 
 def read_blog_post():
+    """
+    Reads the blog posts from a JSON file and returns them as a list of dictionaries.
+
+    Returns:
+        list: A list of dictionaries representing the blog posts.
+    """
     with open('data.json', 'r') as file:
         blog_posts = json.load(file)
     return blog_posts
@@ -29,20 +35,36 @@ def fetch_post_by_id(post_id):
     return None
 
 def update_post(post_id, post_content):
+    """
+       Updates the content of a blog post with the given ID.
+
+       Args:
+           post_id (str): The ID of the blog post to update.
+           post_content (str): The new content for the blog post.
+       """
     blog_posts = read_blog_post()
     for post in blog_posts:
         if post["id"] == post_id:
             post["content"] = post_content
-    with open('data.json', 'w') as f:
-        json.dump(blog_posts, f)
+    with open('data.json', 'w') as file:
+        json.dump(blog_posts, file)
 
 @app.route('/')
 def index():
+    """
+     Renders the index.html template and displays all the blog posts.
+     """
     blog_posts = read_blog_post()
     return render_template('index.html', posts=blog_posts)
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
+    """
+    Adds a new blog post.
+
+    If the request method is GET, renders the add.html template.
+    If the request method is POST, retrieves the form data and adds the new post to the JSON file.
+    """
     if request.method == 'POST':
         blog_posts = read_blog_post()
         author = request.form.get('name')
@@ -63,23 +85,33 @@ def add():
 
 @app.route('/delete/<string:post_id>', methods=['POST'])
 def delete(post_id):
-    flag = True
+    """
+    Deletes a blog post with the given ID.
+
+    Args:
+        post_id (str): The ID of the blog post to delete.
+    """
+    post = fetch_post_by_id(post_id)
+    if post is None:
+        return "Post not found", 404
     blog_posts = read_blog_post()
     for post in blog_posts:
         if post['id'] == post_id:
             blog_posts.remove(post)
-            flag = True
             break
-        else:
-            flag = False
-    if not flag:
-        raise Exception("Sorry, post does not exist")
-    with open('data.json', 'w') as f:
-        json.dump(blog_posts, f)
+    with open('data.json', 'w') as file:
+        json.dump(blog_posts, file)
     return redirect(url_for('index'))
 
 @app.route('/update/<string:post_id>', methods=['GET', 'POST'])
 def update(post_id):
+    """
+      Updates the content of a blog post with the given ID.
+
+      If the post is not found, returns a 404 error.
+      If the request method is GET, renders the update.html template with the blog post data.
+      If the request method is POST, updates the post content in the JSON file.
+      """
     post = fetch_post_by_id(post_id)
     if post is None:
         return "Post not found", 404
@@ -91,14 +123,22 @@ def update(post_id):
 
 @app.route('/like/<string:post_id>', methods=['POST'])
 def like(post_id):
-    with open('data.json', 'r') as f:
-        blog_posts = json.load(f)
-        for post in blog_posts:
-            if post['id'] == post_id:
-                post["likes"] += 1
-                break
-    with open('data.json', 'w') as f:
-        json.dump(blog_posts, f)
+    """
+    Increases the number of likes for a blog post with the given ID.
+
+    Args:
+        post_id (str): The ID of the blog post to like.
+    """
+    post = fetch_post_by_id(post_id)
+    if post is None:
+        return "Post not found", 404
+    blog_posts = read_blog_post()
+    for post in blog_posts:
+        if post['id'] == post_id:
+            post["likes"] += 1
+            break
+    with open('data.json', 'w') as file:
+        json.dump(blog_posts, file)
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
